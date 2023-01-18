710ded483957457066ae44d83ed548687d7711de
BTharungoud <122255721+BTharungoud@users.noreply.github.com>
2023-01-18 22:04:32 +0530

    added

--- counter.js
+++ counter.js
@@ -1,9 +1,12 @@
 function updateCounter() {
     const counter = document.getElementById('counter');
     //increment the counter everytime user visits the page by storing in local storage
-    let count = 1;
-    count = localStorage.getItem('visiterCount');
-    count++;
+    
+    let count = localStorage.getItem('visiterCount');
+    if(count==null)
+        count='1';
+    else
+        count=parseInt(count)+1;
     /**
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
@@ -11,4 +14,4 @@ function updateCounter() {
     localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
 }
-updateCounter();
+updateCounter();  
